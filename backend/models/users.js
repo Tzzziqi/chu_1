474b4726fae2d0cb31edd6a8c47a87ce2247f81a
Freya379 <125752050+Freya379@users.noreply.github.com>
2026-04-06 23:24:59 -0700

    userSchema

---
+++ backend/models/users.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+//====== define UserSchema ======
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true, // clear whitespace
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters'],
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+  },
+  // create this two: createdAt and updatedAt 
+  { timestamps: true } 
+);
+
+// encrypt password before saving
+// pre(save) is a middleware that runs before saving a document. We use it to hash the password before storing it in the database.
+userScheama.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next(); // we need next() here to move on to the next middleware or save the document. If we forget it, the save operation will hang indefinitely.
+});
+
+// compare password for login
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('users', userSchema);
