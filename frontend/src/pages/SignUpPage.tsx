b00ba02ae5ba5834503a287da9b5ae484640b60a
Freya379 <125752050+Freya379@users.noreply.github.com>
2026-04-09 23:06:10 -0700

    page

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,1 +1,6 @@
 // SignUpPage.tsx placeholder
+import AuthForm from '../components/auth/authForm'
+
+export default function SignUpPage() {
+  return <AuthForm mode="signup" />
+}
--- frontend/src/pages/UpdatePasswordPage.tsx
+++ frontend/src/pages/UpdatePasswordPage.tsx
@@ -1,1 +1,6 @@
 // UpdatePasswordPage.tsx placeholder
+import AuthForm from '../components/auth/authForm'
+
+export default function UpdatePasswordPage() {
+  return <AuthForm mode="update-password" />
+}
