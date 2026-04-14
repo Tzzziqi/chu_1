e3375bcfa6069e20eb7dedda6c7ab8a89e0b565e
Freya379 <125752050+Freya379@users.noreply.github.com>
2026-04-13 22:08:18 -0700

    add protected route steup

---
+++ frontend/src/components/common/protectedRoute.tsx
@@ -0,0 +1,21 @@
+import { Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import type { RootState } from '../../store'
+
+interface Props {
+    children: React.ReactNode
+    adminOnly?: boolean
+}
+
+export default function ProtectedRoute({ children, adminOnly = false}: Props) {
+    const { isLoggedIn, user } = useSelector((state: RootState) =>state.auth)
+    if (!isLoggedIn) {
+        return <Navigate to='/signin' />
+    }
+    // in case user is null, so use user?.role to get undefined
+    if (adminOnly && user?.role !== 'admin') {
+        return <Navigate to="/" />
+  }
+  return <> {children} </>
+
+}
